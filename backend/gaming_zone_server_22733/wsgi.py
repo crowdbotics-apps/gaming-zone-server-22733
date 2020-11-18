69fa8a4ecc90b083c98242107d44031414d72655
Crowdbotics <team@crowdbotics.com>
2020-11-18 19:29:01 +0000

    Update models

--- backend/gaming_zone_server_22733/wsgi.py
+++ backend/gaming_zone_server_22733/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaming_zone_server_22733.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaming_zone_server_22733.settings")
 
 application = get_wsgi_application()
